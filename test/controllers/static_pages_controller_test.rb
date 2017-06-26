95acd176d01132dc1895963897a3988d903015bb
codegracie <grace.sojor@yahoo.com>
2017-06-26 18:20:59 +0800

    Add StaticPages Controller and root route

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get static_pages_index_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+  end
+
+end
